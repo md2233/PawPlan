456e16321d8d1b8f171632d290a1e985f147d3a0
Macy <s024275@students.lmsd.org>
2021-03-17 10:24:52 -0400

    start of calendar

--- PawPlan/ContentView.swift
+++ PawPlan/ContentView.swift
@@ -9,10 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var days : [Day] =  [Day(year: "2021", month: "March", day1: "1"),
+            Day(year: "2021", month: "March", day1: "1"),
+            Day(year: "2021", month: "March", day1: "2"),
+           Day(year: "2021", month: "March", day1: "2"),
+            Day(year: "2021", month: "March", day1: "2"),
+           Day(year: "2021", month: "March", day1: "2"),
+           Day(year: "2021", month: "March", day1: "2"),
+               ].sorted{($0.day1 < $1.day1)
+               }
     var body: some View {
-        Text("Bye bye, World!")
-    }
-}
+     ZStack{
+                    //orange background image
+                    Image("background")
+                        .opacity(0.1)
+                    VStack{
+                        //each cardview statement creates one contact with email, name, and profile image
+                        DayView(day: $days[0])
+                        DayView(day: $days[1])
+                        DayView(day: $days[2])
+                        DayView(day: $days[3])
+                        dayView(day: $days[4])
+                        // Before button is pressed, this contact is Prince. When button is pressed, the last contact becomes Tyler.
+                      
+                       
+                        
+                        
+                        Spacer()
+                        
+                    }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                        .padding(.top, 80)
+                    
+                    
+                }
+            }
+        }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
