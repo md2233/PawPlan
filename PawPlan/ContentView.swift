359578a619819a5738b70235d051033150b50843
Macy <s024275@students.lmsd.org>
2021-03-18 10:18:45 -0400

    content view

--- PawPlan/ContentView.swift
+++ PawPlan/ContentView.swift
@@ -9,44 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var days : [Day] =  [Day(year: "2021", month: "March", day1: "1"),
-            Day(year: "2021", month: "March", day1: "1"),
-            Day(year: "2021", month: "March", day1: "2"),
-           Day(year: "2021", month: "March", day1: "2"),
-            Day(year: "2021", month: "March", day1: "2"),
-           Day(year: "2021", month: "March", day1: "2"),
-           Day(year: "2021", month: "March", day1: "2"),
-               ].sorted{($0.day1 < $1.day1)
-               }
+    
+    @State var days : [Day]
+    @State var day : Day
+    
+    
     var body: some View {
-     ZStack{
-                    //orange background image
-                    Image("background")
-                        .opacity(0.1)
-                    VStack{
-                        //each cardview statement creates one contact with email, name, and profile image
-                        DayView(day: $days[0])
-                        DayView(day: $days[1])
-                        DayView(day: $days[2])
-                        DayView(day: $days[3])
-                        dayView(day: $days[4])
-                        // Before button is pressed, this contact is Prince. When button is pressed, the last contact becomes Tyler.
-                      
-                       
-                        
-                        
-                        Spacer()
-                        
-                    }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
-                        .padding(.top, 80)
-                    
-                    
-                }
+        ZStack{
+            NavigationView{
+                List{
+                    ForEach(days.indices, id: \.self){
+                        i in DayView(day:self.$days[i], days: self.$days)
+                    }
+                }.navigationBarTitle("Days")
             }
         }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
